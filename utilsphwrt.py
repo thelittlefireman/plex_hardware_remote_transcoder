76bf3b3d0d9ebf454b82c771cdcaa07949286557
thomas goureau <thomas.goureau@gmail.com>
2017-10-15 17:05:03 +0200

    add plex ffmpeg info, fix ffmpeg verbose and some parameters

--- utilsphwrt.py
+++ utilsphwrt.py
@@ -40,8 +40,8 @@ ffmpegTranscoderChangeList = [
 ]"""
 #????? -manifest_name http://127.0.0.1:32400/video/:/transcode/session/8frivkc33m10k3zrelaj5lsk/6ffe3670-ebe1-48f8-b326-b30e4bd0b94c/manifest
 paramsToDelete=["-map_inlineass","-filter_complex","-map","-loglevel_plex","-fdash"]
-paramsToChange=["-time_delta","-channel_layoutstereo","-skip_to_segment","-progressurl","-codec:1","-codec:#0x02","-codec:0","-codec:#0x01","ac3","libx264","h264","-crf:0","-maxrate:0","-bufsize:0","-r:0","-preset:0","-x264opts:0","-force_key_frames:0","-metadata:s:1","-metadata:s:0","aac","-ar:1","-channel_layout:1","-b:1","-segment_copyts","expr:gte(t0+n_forced*1)","ass"]
-newParams=["-segment_time_delta","","-segment_start_number","-progress","-codec:a","-codec:a","-codec:v","-codec:v","copy","cedrus264 -pix_fmt nv12","cedrus264 -pix_fmt nv12","-crf","-maxrate","-bufsize","-r","-preset","-x264opts","-force_key_frames","-metadata:s:a","-metadata:s:v","copy","-ar","-channel_layout","-b:a","-mpegts_copyts","expr:gte(t,n_forced*1)","copy"]
+paramsToChange=["-time_delta","-channel_layoutstereo","-skip_to_segment","-progressurl","ac3","libx264","h264","-crf:0","-maxrate:0","-bufsize:0","-r:0","-preset:0","-x264opts:0","-force_key_frames:0","-metadata:s:1","-metadata:s:0","aac","-ar:1","-channel_layout:1","-b:1","-segment_copyts","expr:gte(t0+n_forced*1)","ass"]
+newParams=["-segment_time_delta","","-segment_start_number","-progress","copy","cedrus264 '-pix_fmt nv12'","cedrus264 '-pix_fmt nv12'","-crf","-maxrate","-bufsize","-r","-preset","-x264opts","-force_key_frames","-metadata:s:a","-metadata:s:v","copy","-ar","-channel_layout","-b:a","-mpegts_copyts","expr:gte(t,n_forced*1)","copy"]
 #TODO subtitle : -vf subtitles=sub.srt:force_style='FontName=DejaVu Serif,FontSize=24' or ass codec
 
 
